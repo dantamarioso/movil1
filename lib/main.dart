d8d560701d9552870e89728c8d783cb7527a0f4d
Mario <dantamarioso@gmail.com>
2025-04-07 16:54:00 -0500

    usuarios

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,32 @@ class Database {
     port: 3306,
     user: 'dart_user',
     password: '1234',
-    db: 'inversiones',
+    db: 'movil',
   );
 
   // Método para obtener una conexión a la base de datos
-  Future<void> testConnection() async {
+  Future<List<Map<String, dynamic>>> getConnection() async {
+    final conn = await MySqlConnection.connect(settings);
+    print('Conexión exitosa');
     try {
-      final conn = await MySqlConnection.connect(settings);
-      print('Conexión exitosa a la base de datos');
+      var results = await conn.query('SELECT * FROM usuarios');
+      return results.map((row) => row.fields).toList();
+    } finally {
       await conn.close();
-    } catch (e) {
-      print('Error al conectar a la base de datos: $e');
     }
   }
 }
 
 void main() async {
   var db = Database();
-  await db.testConnection();
+
+  try {
+    var productos = await db.getConnection();
+    print('Usuarios obtenidos:');
+    for (var producto in productos) {
+      print('$producto');
+    }
+  } catch (e) {
+    print('Error al obtener los usuarios: $e');
+  }
 }
